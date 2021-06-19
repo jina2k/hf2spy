45ed805d3009348de6cfbe451ad41c7e7d834df1
jina2k <jina26p@gmail.com>
2021-06-19 14:05:36 -0400

    setting up console app

---
+++ xmlproject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace xmlproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
